d42568519cfe700c81f928dc4c3dfbfc3d6995bf
OutputTarou <ynagao1230@gmail.com>
2023-11-19 07:56:19 +0900

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+// flutter
 import 'package:flutter/material.dart';
+// packages
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:udemy_sns/main_model.dart';
+import 'package:firebase_core/firebase_core.dart';
+// models
+import 'main_model.dart';
+// options
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
